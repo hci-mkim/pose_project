d6e3b9b79f18a9a56861d5cd8fcda642020693f5
hci-mkim <min1106.kim@gmail.com>
2021-04-19 20:13:50 +0900

    thrid commit

--- setup.py
+++ setup.py
@@ -1,24 +1,15 @@
 # -*- coding: utf-8 -*-
-"""
-Setup script.
-Created on Wed Sep 18 10:00:00 2019
-Author: Prasun Roy | CVPRU-ISICAL (http://www.isical.ac.in/~cvpr)
-GitHub: https://github.com/prasunroy/rcnnpose-pytorch
-
-"""
-
-
 from setuptools import setup, find_packages
 
 
 setup(name='rcnnpose-pytorch',
       version='0.1.0',
-      description='Mask R-CNN and Keypoint R-CNN wrapper for pose estimation with PyTorch',
+      description='Extracting Joint coordinates & Angles',
       long_description=open('README.md').read(),
       long_description_content_type='text/markdown',
-      author='Prasun Roy',
-      author_email='prasunroy.pr@gmail.com',
-      url='https://github.com/prasunroy/rcnnpose-pytorch',
+      author='Min Kim',
+      author_email='min1106.kim@gmail.com',
+      url='https://github.com/hci-mkim/pose_project.git',
       license='MIT',
       install_requires=[
               'numpy',
